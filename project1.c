b7a69e140d11c6dd09e4fb98d0880453227bd886
mahima68 <133637096+mahima68@users.noreply.github.com>
2023-05-15 13:24:57 +0530

    Add files via upload

---
+++ project1.c
@@ -0,0 +1,29 @@
+//guess a number (CODE WITH HARRY)
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+int main() {
+    int number, guess, nguesses =1;
+    srand(time(0)); //different no
+    number = rand()% 100 + 1; //generates random no
+    //printf("The number is %d\n" , number);
+
+    do{
+        printf("Guess the number between 1 to 100\n");
+        scanf("%d" , &guess);
+        if(guess>number) {
+            printf("Lower number please\n");
+        }
+        else if(guess<number) {
+        printf("Higher number please\n");
+    }
+    else {
+        printf("You guessed it in %d attenpts\n", nguesses);
+    }
+    nguesses++;
+    } while(guess!=number);
+    return 0;
+}
+ 
